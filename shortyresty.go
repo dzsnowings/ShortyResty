66bf9d3ea545c721da5b2af97a83a54cf2448da6
Danielle Zhao <dzsnowings@gmail.com>
2021-02-26 01:42:54 -0500

    Add shortyresty.go via upload

---
+++ shortyresty.go
@@ -0,0 +1,101 @@
+package main
+
+// Import necessary packages
+import (
+    "log"
+    "net/http"
+    "net/url"
+    "encoding/json"
+	"math/rand"
+)
+
+// LongUrl formats long URLs for easy conversion to and from JSON
+type LongUrl struct {
+    Long_url string `json:"url"`
+}
+
+// ShortUrl formats short URLs for easy converstion to and from JSON
+type ShortUrl struct {
+	Short_url string `json:"short_url"`
+}
+
+// Map of shortened URLs keyed by their ID
+var urlMap map[string]LongUrl
+// Constant string containing possible characters for shortened URL IDs
+const chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+// The main function creates the urlMap, handles HTTP requests, and starts a web server on port 8080
+func main() {
+	urlMap = make(map[string]LongUrl)
+	http.HandleFunc("/shorten", handleShorten)		// Go to handleShorten if URL has endpoint "/shorten"
+	http.HandleFunc("/$ID", handleRedirect)		// Go to handleRedirect if URL has endpoint "/%ID"
+	log.Fatal(http.ListenAndServe(":8080", nil))		// Host web server on port 8080
+}
+
+// The handleShorten function takes in a long URL, shortens it into a ShortUrl,
+// tracks the long and short URL pairs in a map, and responds with the shortened URL in JSON
+func handleShorten(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")		// Set the return Content-Type as JSON
+	w.WriteHeader(http.StatusCreated)
+
+	var longUrl LongUrl
+	json.NewDecoder(r.Body).Decode(&longUrl)        // Decode the POST request body from JSON into a LongUrl
+
+	_, err := url.ParseRequestURI(longUrl.Long_url)
+	if err != nil {
+	    w.Write([]byte(longUrl.Long_url + " is not a valid URL."))      // Respond with error message if longUrl is not a valid URL
+	    return
+	}
+
+	var id string
+	var exists bool = true
+    // While id exists in urlMap, keep generating random IDs
+	for exists {
+	    id = generateID()
+	    if _, key := urlMap[id]; !key {
+            exists = false
+        }
+	}
+	urlMap[id] = longUrl        // Add id: longUrl to urlMap
+	
+	var urlStr string = "http://127.0.0.1:8080/" + id       // Create the shortened url
+	shortUrl := ShortUrl{urlStr}
+	json.NewEncoder(w).Encode(shortUrl)     // Encode the response from a ShortUrl into JSON
+}
+
+// The handleRedirect function takes in a short URL's ID, finds its corresponding LongUrl
+// using the urlMap, and redirects to the long form of the URL
+func handleRedirect(w http.ResponseWriter, r *http.Request) {
+    var shortUrl ShortUrl
+	json.NewDecoder(r.Body).Decode(&shortUrl)       // Decode the GET request body from JSON into a ShortUrl
+
+	_, err := url.ParseRequestURI(shortUrl.Short_url)
+    if err != nil {
+        w.Write([]byte(shortUrl.Short_url + " is not a valid URL."))        // Respond with error message if shortUrl is not a valid URL
+        return
+    }
+
+    var id string = shortUrl.Short_url[len(shortUrl.Short_url) - 8:]        // Get ID of the ShortUrl
+    if _, key := urlMap[id]; key {
+        http.Redirect(w, r, urlMap[id].Long_url, 302)       // 302 redirect to the long URL
+    } else {
+        w.Write([]byte(shortUrl.Short_url + " is not linked to a long URL."))       // Respond with error message if id is not in urlMap
+        return
+    }
+}
+
+// The generateID function generates and returns a random string of length 8 to use as the ID
+// for a shortened URL
+func generateID() string {
+    var id string = ""
+    for i := 0; i < 8; i++ {
+   		id += string(chars[rand.Intn(len(chars))])
+   	}
+   	return id
+}
+
+/*
+For testing purposes:
+curl -v -H "Content-Type: application/json" -X POST -d "{\"url\": \"http://example.com/verylonguselessURLthatdoesnotseemtoend/123\"}" http://127.0.0.1:8080/shorten
+curl -v -H "Content-Type: application/json" -X GET -d "{\"short_url\": \"http://127.0.0.1:8080/rfBd56ti\"}" http://127.0.0.1:8080/$ID
+*/
